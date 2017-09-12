b31f807687667d381017bc6ef3715010507c79bf
Mirko Da Corte <mirko.dacorte@digicando.com>
2017-09-12 18:23:10 +0200

    Updated splitter

--- splitter/contracts/Splitter.sol
+++ splitter/contracts/Splitter.sol
@@ -4,9 +4,21 @@ contract Splitter {
     // Fields.
     address public bobAddress;
     address public carolAddress;
+    bool public isKilled;
+    address public owner;
 
     // Events.
-    event SimpleSplit(uint ammount);
+    event SplitEvent(
+        address indexed sender,
+        address indexed receiver1,
+        address indexed receiver2,
+        uint ammount);
+
+    // Modifiers.
+    modifier restricted() {
+        if (msg.sender == owner)
+            _;
+    }
 
     // Costructor.
     function Splitter(
@@ -15,6 +27,8 @@ contract Splitter {
     ) {
         bobAddress = _bobAddress;
         carolAddress = _carolAddress;
+
+        owner = msg.sender;
     }
 
     // Functions.
@@ -26,13 +40,32 @@ contract Splitter {
         return carolAddress.balance;
     }
 
+    function kill() restricted {
+        isKilled = true;
+    }
+
+    function split(address receiver1, address receiver2) payable {
+        //requisites
+        require(!isKilled);
+
+        //handle case of odd msg.value (why handle 0.5 wei?)
+        uint receiver1Value = msg.value / 2;
+        uint receiver2Value = msg.value - receiver1Value;
+
+        //events
+        SplitEvent(
+            msg.sender,
+            receiver1,
+            receiver2,
+            msg.value);
+
+        //split
+        receiver1.transfer(receiver1Value);
+        receiver2.transfer(receiver2Value);
+    }
+
     //deafult
     function () payable {
-        //handle case of odd msg.value
-        uint bobValue = msg.value / 2;
-        uint carolValue = msg.value - bobValue;
-
-        bobAddress.transfer(bobValue);
-        carolAddress.transfer(carolValue);
+        split(bobAddress, carolAddress);
     }
 }

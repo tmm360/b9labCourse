d225a10a6c958b35861d2acfa227ca2334a0f9dc
Mirko Da Corte <mirko.dacorte@digicando.com>
2017-09-16 19:56:11 +0200

    Updated tests

--- remittance/test/remittanceTest.js
+++ remittance/test/remittanceTest.js
@@ -2,22 +2,24 @@ var Remittance = artifacts.require("./Remittance.sol");
 var keccak256 = obj => "0x" + require('js-sha3').keccak256(obj);
 
 contract('Remittance', accounts => {
-    var instance;
+    var remittance;
 
     beforeEach(() => Remittance.new()
-        .then(_instance => instance = _instance));
+        .then(_instance => remittance = _instance));
 
-    it("Should set owner", () =>
+    it("Should set owner", () => {
+        var instance = remittance;
         instance.owner.call({ from: accounts[0] })
             .then(result => assert.equal(result, accounts[0], "Owner is wrong"))
-    );
+    });
 
     it("Should update change password hash", () => {
+        var instance = remittance;
         var hash1 = keccak256("test");
         var hash2 = keccak256("test2");
         var deposit;
 
-        instance.deposit(hash1, 12 /*h*/, { from: accounts[0], value: 1000 })
+        instance.deposit(hash1, 12, { from: accounts[0], value: 1000 })
             .then(txInfo => instance.deposits.call(accounts[0], hash1, { from: accounts[0] }))
             .then(_deposit => {
                 deposit = _deposit;
@@ -28,11 +30,32 @@ contract('Remittance', accounts => {
                 assert.deepEqual(_deposit, deposit, "Should be moved");
                 return instance.deposits.call(accounts[0], hash1, { from: accounts[0] });
             })
-            .then(_deposit => assert.deepEqual(_deposit, [new web3.BigNumber(0), new web3.BigNumber(0), new web3.BigNumber(0)],
-                "Previous should be setted to 0"));
+            .then(_deposit => assert.deepEqual(_deposit,
+                [new web3.BigNumber(0), new web3.BigNumber(0), new web3.BigNumber(0)],
+                "Should set to 0 on prev hash"));
     });
 
-    it("Should kill from owner", () =>
+    it("Should deposit new founds", () => {
+        var instance = remittance;
+        var hash1 = keccak256("test");
+        var blockTimestamp;
+
+        instance.deposit(hash1, 12, { from: accounts[0], value: 1000 })
+            .then(txInfo => {
+                var block = web3.eth.getBlock(txInfo.receipt.blockNumber);
+                blockTimestamp = block.timestamp;
+
+                return instance.deposits.call(accounts[0], hash1, { from: accounts[0] });
+            })
+            .then(_deposit => assert.deepEqual(_deposit,
+                [new web3.BigNumber(990 /* 1000 - 1% */),
+                 new web3.BigNumber(blockTimestamp),
+                 new web3.BigNumber(blockTimestamp + 43200 /* +12h */)],
+                "Should set deposit"));
+    });
+
+    it("Should kill from owner", () => {
+        var instance = remittance;
         instance.isKilled.call({ from: accounts[0] })
             .then(result => {
                 assert.isFalse(result, "Should not be killed");
@@ -40,9 +63,10 @@ contract('Remittance', accounts => {
             })
             .then(txInfo => instance.isKilled.call({ from: accounts[0] }))
             .then(result => assert.isTrue(result, "Should be killed"))
-    );
+    });
 
-    it("Should not kill from not owner", () =>
+    it("Should not kill from not owner", () => {
+        var instance = remittance;
         instance.isKilled.call({ from: accounts[0] })
             .then(result => {
                 assert.isFalse(result, "Should not be killed");
@@ -50,5 +74,5 @@ contract('Remittance', accounts => {
             })
             .then(txInfo => instance.isKilled.call({ from: accounts[0] }))
             .then(result => assert.isFalse(result, "Should not be killed"))
-    );
+    });
 })
--- splitter/test/splitterTest.js
+++ splitter/test/splitterTest.js
@@ -5,26 +5,29 @@ var bobAddress = "0xBeB2A06Eb48AcDd0e4C23B6740E04c495856fA08";
 var carolAddress = "0x79D5D78b75469d06f06ce56A69890eDe014112E9";
 
 contract('Splitter', accounts => {
-    var instance;
+    var splitter;
 
     beforeEach(() => Splitter.new(bobAddress, carolAddress)
-        .then(_instance => instance = _instance));
+        .then(_instance => splitter = _instance));
 
-    it("Should set Bob and Carol addresses", () =>
+    it("Should set Bob and Carol addresses", () => {
+        var instance = splitter;
         instance.bobAddress.call({ from: accounts[0] })
             .then(result => {
                 assert.equal(result, bobAddress.toLowerCase(), "Bob address is wrong");
                 return instance.carolAddress.call({ from: accounts[0] });
             })
             .then(result => assert.equal(result, carolAddress.toLowerCase(), "Carol address is wrong"))
-    );
+    });
 
-    it("Should set owner", () =>
+    it("Should set owner", () => {
+        var instance = splitter;
         instance.owner.call({ from: accounts[0] })
             .then(result => assert.equal(result, accounts[0], "Owner is wrong"))
-    );
+    });
 
-    it("Should kill from owner", () =>
+    it("Should kill from owner", () => {
+        var instance = splitter;
         instance.isKilled.call({ from: accounts[0] })
             .then(result => {
                 assert.isFalse(result, "Should not be killed");
@@ -32,9 +35,10 @@ contract('Splitter', accounts => {
             })
             .then(txInfo => instance.isKilled.call({ from: accounts[0] }))
             .then(result => assert.isTrue(result, "Should be killed"))
-    );
+    });
 
-    it("Should not kill from not owner", () =>
+    it("Should not kill from not owner", () => {
+        var instance = splitter;
         instance.isKilled.call({ from: accounts[0] })
             .then(result => {
                 assert.isFalse(result, "Should not be killed");
@@ -42,9 +46,10 @@ contract('Splitter', accounts => {
             })
             .then(txInfo => instance.isKilled.call({ from: accounts[0] }))
             .then(result => assert.isFalse(result, "Should not be killed"))
-    );
+    });
 
     it("Should split with addresses", () => {
+        var instance = splitter;
         var address1 = "0x2836C2B0fBf18Bc8e889Ba4782d76fAFed5cfC13";
         var address2 = "0xabCEbb26FFC6dF88b0D2E7F73f00f7883D1bfd5d";
 
@@ -57,14 +62,15 @@ contract('Splitter', accounts => {
             .then(result => assert.equal(result, 500, "Address2 balance is wrong"))
     });
 
-    it("Should not split if killed", () =>
+    it("Should not split if killed", () => {
+        var instance = splitter;
         instance.kill({ from: accounts[0] })
             .then(txInfo => expectedExceptionPromise(() =>
                 instance.sendTransaction({ from: accounts[0], value: 1000, gas: 3000000 }), 3000000))
-    );
+    });
 
-    it("Should withdraw owned balance", () =>
-    {
+    it("Should withdraw owned balance", () => {
+        var instance = splitter;
         var accountBalanceStep0 = web3.eth.getBalance(accounts[1]);
         var accountBalanceStep1;
         var accountBalanceStep2;
@@ -91,7 +97,8 @@ contract('Splitter', accounts => {
             })
     });
 
-    it("Should split with fallback", () =>
+    it("Should split with fallback", () => {
+        var instance = splitter;
         instance.sendTransaction({ from: accounts[0], value: 1000 })
             .then(txInfo => instance.getBalance.call(bobAddress, { from: accounts[0] }))
             .then(result => {
@@ -99,5 +106,5 @@ contract('Splitter', accounts => {
                 return instance.getBalance.call(carolAddress, { from: accounts[0] });
             })
             .then(result => assert.equal(result, 500, "Carol balance is wrong"))
-    );
+    });
 })

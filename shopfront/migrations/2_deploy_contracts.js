424a5977c9aa9189627f9345469163c30d38b7cd
Mirko Da Corte <mirko.dacorte@digicando.com>
2017-10-09 03:56:44 +0200

    Updated deploy and removed SimpleStorage

--- shopfront/migrations/2_deploy_contracts.js
+++ shopfront/migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var CollectHub = artifacts.require("./CollectHub.sol");
+var MetaCoin = artifacts.require("./MetaCoin.sol");
+var Shopfront = artifacts.require("./Shopfront.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+module.exports = async function(deployer) {
+  await deployer.deploy(MetaCoin);
+  await deployer.deploy(Shopfront);
+  await deployer.deploy(CollectHub, Shopfront.address);
 };

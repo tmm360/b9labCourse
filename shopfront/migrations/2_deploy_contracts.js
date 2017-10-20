a319489335e9a2bc6ef66673f2fc0014788c52a5
Mirko Da Corte <mirko.dacorte@digicando.com>
2017-10-20 19:49:57 +0200

    Fixed deploy

--- shopfront/migrations/2_deploy_contracts.js
+++ shopfront/migrations/2_deploy_contracts.js
@@ -2,8 +2,8 @@ var CollectHub = artifacts.require("./CollectHub.sol");
 var MetaCoinERC20 = artifacts.require("./MetaCoinERC20.sol");
 var Shopfront = artifacts.require("./Shopfront.sol");
 
-module.exports = async function(deployer) {
-  await deployer.deploy(MetaCoinERC20);
-  await deployer.deploy(Shopfront, MetaCoinERC20.address);
-  await deployer.deploy(CollectHub, Shopfront.address);
+module.exports = function(deployer) {
+  deployer.deploy(MetaCoinERC20).then(() =>
+    deployer.deploy(Shopfront, MetaCoinERC20.address).then(() =>
+      deployer.deploy(CollectHub, Shopfront.address)));
 };
